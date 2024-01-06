4921ab981669c2e3a648fcc1629c29ae065bd761
Pham Duc Tien <ptiencntt94@gmail.com>
2024-01-07 02:31:09 +0700

    Change navbar in mobile

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,75 +1,74 @@
-<?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/models/user.php');
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/user.php';
 ?>
 
 <div class="site-header">
-    <nav class="uk-navbar-container">
-        <div class="uk-container">
-            <div uk-navbar>
+	<nav class="uk-navbar-container">
+		<div class="uk-container">
+			<div uk-navbar>
 
-                <div class="uk-navbar-left">
-                    <!-- <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">Logo</a> -->
+				<div class="uk-navbar-left">
+					<!-- <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">Logo</a> -->
 
-                    <ul class="uk-navbar-nav uk-visible@s">
-                        <li class="menu-item uk-active">
-                            <a href="<?= SITE_URL; ?>"><span class="uk-margin-small-right" uk-icon="home"></span>Dashboard</a>
-                        </li>
-                        <?php if (is_user_logged_in()) { ?>
-                            <li class="menu-item">
-                                <a href="<?php echo SITE_URL. '/index.php?controller=links'; ?>"><span class="uk-margin-small-right" uk-icon="link"></span>Tạo Link <span uk-navbar-parent-icon></span></a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li><a href="<?php echo SITE_URL. '/index.php?controller=links&action=list'; ?>"><span class="uk-margin-small-right" uk-icon="list"></span>Danh sách Link</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="menu-item">
-                                <a href="<?php echo SITE_URL. '/index.php?controller=user'; ?>"><span class="uk-margin-small-right" uk-icon="users"></span>Thành viên</a>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                    <a href="#" class="uk-navbar-toggle uk-hidden@s" uk-navbar-toggle-icon uk-toggle="target: #sidenav"></a>
+					<ul class="uk-navbar-nav uk-visible@s">
+						<li class="menu-item uk-active">
+							<a href="<?php echo SITE_URL; ?>"><span class="uk-margin-small-right" uk-icon="home"></span>Dashboard</a>
+						</li>
+						<li class="menu-item">
+							<a href="<?php echo SITE_URL . '/index.php?controller=links'; ?>"><span class="uk-margin-small-right" uk-icon="link"></span>Tạo Link <span uk-navbar-parent-icon></span></a>
+							<div class="uk-navbar-dropdown">
+								<ul class="uk-nav uk-navbar-dropdown-nav">
+									<li><a href="<?php echo SITE_URL . '/index.php?controller=links&action=list'; ?>"><span class="uk-margin-small-right" uk-icon="list"></span>Danh sách Link</a></li>
+								</ul>
+							</div>
+						</li>
+						<li class="menu-item">
+							<a href="<?php echo SITE_URL . '/index.php?controller=user'; ?>"><span class="uk-margin-small-right" uk-icon="users"></span>Thành viên</a>
+						</li>
+					</ul>
+					<a href="#" class="uk-navbar-toggle uk-hidden@s" uk-navbar-toggle-icon uk-toggle="target: #sidenav"></a>
 
-                </div>
+				</div>
 
-                <div class="uk-navbar-right">
-                    <?php if (is_user_logged_in()) { 
-                        $user = User::get_current_user();
-                        ?>
-                        <div class="uk-navbar-item">
-                            <div class="uk-flex-inline uk-flex-middle" style="gap: 4px;">
-                                <span uk-icon="user"></span><span><?= $user['username']; ?>,</span> <a href="<?= SITE_URL ?>/index.php?controller=login&action=logout">Thoát</a>
-                            </div>
-                        </div>
-                    <?php } else { ?>
-                        <a class="uk-button uk-button-primary uk-button-small uk-flex uk-flex-middle" href="<?php echo SITE_URL. '/index.php?controller=login'; ?>">
-                            <span class="uk-margin-small-right" uk-icon="user"></span>
-                            <span>Đăng nhập</span>
-                        </a>
-                    <?php } ?>
-                </div>
+				<div class="uk-navbar-right">
+					<?php
+					if ( is_user_logged_in() ) {
+						$user = User::get_current_user();
+						?>
+						<div class="uk-navbar-item">
+							<div class="uk-flex-inline uk-flex-middle" style="gap: 4px;">
+								<span uk-icon="user"></span><span><?php echo $user['username']; ?>,</span> <a href="<?php echo SITE_URL; ?>/index.php?controller=login&action=logout">Thoát</a>
+							</div>
+						</div>
+					<?php } else { ?>
+						<a class="uk-button uk-button-primary uk-button-small uk-flex uk-flex-middle" href="<?php echo SITE_URL . '/index.php?controller=login'; ?>">
+							<span class="uk-margin-small-right" uk-icon="user"></span>
+							<span>Đăng nhập</span>
+						</a>
+					<?php } ?>
+				</div>
 
-            </div>
-        </div>
-    </nav>
+			</div>
+		</div>
+	</nav>
 </div>
-<div id="sidenav" uk-offcanvas="flip: false" class="uk-offcanvas">
-    <div class="uk-offcanvas-bar">
-        <ul class="uk-nav">
-            <li class="menu-item uk-active">
-                <a href="<?= SITE_URL; ?>"><span class="uk-margin-small-right" uk-icon="home"></span>Dashboard</a>
-            </li>
-            <?php if (is_user_logged_in()) { ?>
-                <li class="menu-item">
-                    <a href="<?php echo SITE_URL. '/index.php?controller=links'; ?>"><span class="uk-margin-small-right" uk-icon="link"></span>Tạo Link</a>
-                </li>
-                <li class="menu-item">
-                    <a href="<?php echo SITE_URL. '/index.php?controller=links&action=list'; ?>"><span class="uk-margin-small-right" uk-icon="list"></span>Danh sách Link</a>
-                </li>
-                <li class="menu-item">
-                    <a href="#"><span class="uk-margin-small-right" uk-icon="settings"></span>Thống Kê</a>
-                </li>
-            <?php } ?>
-        </ul>
-    </div>
+<div id="sidenav" uk-offcanvas="flip: false">
+	<div class="uk-offcanvas-bar">
+		<ul class="uk-nav-default" uk-nav>
+			<li class="menu-item uk-active">
+				<a href="<?php echo SITE_URL; ?>"><span class="uk-margin-small-right" uk-icon="home"></span>Dashboard</a>
+			</li>
+			<li class="menu-item uk-parent">
+				<a href="<?php echo SITE_URL . '/index.php?controller=links'; ?>"><span class="uk-margin-small-right" uk-icon="link"></span>Tạo Link<span uk-nav-parent-icon></span></a>
+				<ul class="uk-nav-sub">
+					<li class="menu-item">
+						<a href="<?php echo SITE_URL . '/index.php?controller=links&action=list'; ?>"><span class="uk-margin-small-right" uk-icon="list"></span>Danh sách Link</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu-item">
+				<a href="<?php echo SITE_URL . '/index.php?controller=user'; ?>"><span class="uk-margin-small-right" uk-icon="users"></span>Thành viên</a>
+			</li>
+		</ul>
+	</div>
 </div>
